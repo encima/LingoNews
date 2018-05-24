b07e4b8b2649de4893dc78461cfc444b77765567
encima <encima@gmail.com>
2018-05-24 22:50:50 +0200

    cleaned up output

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ extract(url).then((article) => {
 	console.log(formatted);
   translate(formatted, {from: from, to: to}).then(res => {
       console.log(res.text);
-      //=> Ik spreek Nederlands!
-      console.log(res.from.text.autoCorrected);
-      //=> true
-      console.log(res.from.text.value);
-      //=> I [speak] Dutch!
-      console.log(res.from.text.didYouMean);
-      //=> false
   }).catch(err => {
       console.error(err);
   });
@@ -32,10 +25,4 @@ extract(url).then((article) => {
   console.log(err);
 });
 
-const deutsch = `Vielen Dank. Deutsch ist seht schwerig, eh? :P 
-Ich habe nur dir formale gelernt, mit einigen Ausnahmen. Mit dem Hund, ich bin viele Stunden draussen jetzt. Was machen sie normalerweise nach der Arbeit? \
-Ich habe Munchen geliebt, so klingt Hamburg fantastisch! Florenz ist cool auch aber ich war noch nie in Barcelona. Jedenfalls noch nicht! \
-Die Gotthard tunnel war schrecklich. Stopp und Start (?) fur viele Stunden. 
-Wie war's mit Dienstag nachste Woche? Nach der Arbeit. Mit Sara und Astrid oder nicht, es stört mich nicht.a
-Gehst du an jeder Schule zum Unterricht fur eine Probezeit?`;
 
