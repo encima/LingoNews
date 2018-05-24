020d4aad3c1165fd0bfafbdae7697dcc9601757a
encima <encima@gmail.com>
2018-05-24 23:06:14 +0200

    arguments added

--- index.js
+++ index.js
@@ -3,20 +3,44 @@ const {
   extract
 } = require('article-parser');
 const striptags = require('striptags');
+const ArgumentParser = require('argparse').ArgumentParser;
+const parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'LingoNews - Translate text in the command line'
+});
+
 let url = 'http://www.nachrichtenleicht.de/tom-wolfe-gestorben.2045.de.html?dram:article_id=418124';
 let from = 'de';
 let to = 'en';
 
-if (process.argv.length > 2) {
-	url = process.argv[2];
-	from
-}
+parser.addArgument(
+    [ '-u', '--url' ],
+    {
+			action: 'store',
+    	help: 'URL to extract the body from'
+    }
+);
+parser.addArgument(
+    [ '-f', '--from' ],
+    {
+			action: 'store',
+      help: 'Language code to translate from (i.e. EN)'
+     }
+);
+parser.addArgument(
+  ['-t', '--to'],
+  {
+		action: 'store',
+    help: 'Language code to translate to (i.e. DE)'
+  }
+);
+const args = parser.parseArgs();
 
-
-extract(url).then((article) => {
+extract(args.url || url).then((article) => {
   const formatted = striptags(article.content);
 	console.log(formatted);
-  translate(formatted, {from: from, to: to}).then(res => {
+  translate(formatted, {from: args.from || from, to: args.to || to}).then(res => {
       console.log(res.text);
   }).catch(err => {
       console.error(err);
